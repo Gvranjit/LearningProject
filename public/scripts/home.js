d0837913f9a22913dc210c3d01ffd24b97cf55e3
Gaurav Ranjit <gvranjit@gmail.com>
2021-03-01 16:16:42 +0545

    Add chat feature frontend

--- public/scripts/home.js
+++ public/scripts/home.js
@@ -1,4 +1,37 @@
 const socket = io();
+const messagebox = document.getElementById("messagebox");
+
+const inputBox = document.getElementById("chatInputBox");
+
+//Input listnen
+chatInput.addEventListener("submit", () => {
+     console.log("you entered");
+     messagebox.innerHTML += ` <div class="card message">
+     <div class="card-header ">
+     <div class="blue"> ${username}
+     </div> : ${inputBox.value}
+     </div>
+    
+  </div>`;
+     messagebox.scrollTop = messagebox.scrollHeight;
+     socket.emit("message", { sender: username, content: inputBox.value });
+     inputBox.value = null;
+});
+//Socket listners
+
 socket.on("connect", () => {
      console.log("Connect to socket");
+
+     messagebox.scrollTop = messagebox.scrollHeight;
+});
+
+socket.on("message", (message) => {
+     messagebox.innerHTML += ` <div class="card message">
+     <div class="card-header ">
+     <div class="blue"> ${message.sender}
+     </div> : ${message.content}
+     </div>
+    
+  </div>`;
+     messagebox.scrollTop = messagebox.scrollHeight;
 });
